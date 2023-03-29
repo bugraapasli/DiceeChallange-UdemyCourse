49dd6ef0c8bc7f41d48e0c04125705998c40bb05
Buğra Paslı <73783239+bugraapasli@users.noreply.github.com>
2023-03-30 02:20:49 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-var randomNumber1 = Math.floor(Math.random()*6) + 1;
-var randomNumber2 = Math.floor(Math.random()*6) + 1;
+var randomNumber1 = Math.floor(Math.random()*6) + 1;  // random number genarating for dice between 1-6.
+var randomNumber2 = Math.floor(Math.random()*6) + 1; // random number genarating for dice between 1-6.
 
-var gettingElements = document.querySelectorAll("img");
+var gettingElements = document.querySelectorAll("img");  
 
-var image1 = "dice" + randomNumber1 + ".png";
+var image1 = "dice" + randomNumber1 + ".png";  // dice1-dice6.png
 var image2 = "dice" + randomNumber2 + ".png";
 
 gettingElements[0].setAttribute("src",image1);
 gettingElements[1].setAttribute("src", image2);
 
-if(image1 === image2){
+if(image1 === image2){                                             // condition parts for changing the website
     document.querySelector("h1").textContent = "Draw Game!";
 }
 else if(image1 > image2){
